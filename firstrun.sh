b823b430ae66c4d155285a3077a316435bbcd308
Sean Thomas Williams <seanthomaswilliams@gmail.com>
2012-02-12 16:49:55 -0500

    Added firstrun.sh to bootstrap new environments.

---
+++ firstrun.sh
@@ -0,0 +1,4 @@
+sudo yum -y install zsh dstat git
+wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+git config --global user.name "Sean Thomas Williams"
+git config --global user.email seanthomaswilliams@gmail.com
