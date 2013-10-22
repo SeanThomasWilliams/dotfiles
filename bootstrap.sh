f83c07770778c474327cbcebdfbd09f323050afa
Sean Williams <swilliams@woti.com>
2013-10-22 14:20:39 -0400

    Fixup for go

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,16 +7,6 @@ for file in `echo .*`; do
     fi
 done
 
-gover="go1.0.3.linux-amd64"
-
-if [[ ! -e "$HOME/go" ]]; then
-    mkdir -p install
-    cd install
-    wget -N "https://go.googlecode.com/files/$gover.tar.gz"
-    tar -xvf $gover.tar.gz -C ~/
-    cd -
-fi
-
 ln -sf `pwd`/vimrc ~/.vimrc
 ln -sf `pwd`/vim ~/.vim
 
@@ -24,5 +14,3 @@ git submodule init
 git submodule update
 
 bash -c "go get -u github.com/nsf/gocode"
-bash -c "go get -u github.com/vmihailenco/redis"
-
