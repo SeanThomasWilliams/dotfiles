ad8e71238b52e44c79db4fc2a9b80eb2965c3cce
Sean Williams <seanthomaswilliams@gmail.com>
2016-01-18 12:12:49 -0500

    Fixup installers

--- scripts/installtmux.sh
+++ scripts/installtmux.sh
@@ -11,7 +11,7 @@ cd $HOME/software
 wget https://github.com/libevent/libevent/releases/download/release-2.0.22-stable/libevent-2.0.22-stable.tar.gz
 tar -xvf libevent-2.0.22-stable.tar.gz
 cd libevent-2.0.22-stable
-./configure --prefix=/usr
+./configure --prefix=/usr/local
 make -j
 sudo make install
 
@@ -21,7 +21,7 @@ wget https://github.com/tmux/tmux/releases/download/2.1/tmux-2.1.tar.gz
 tar -xvf tmux-2.1.tar.gz
 cd tmux-2.1
 make configure
-./configure --prefix=/usr
+./configure --prefix=/usr/
 make -j
 sudo make install
 
