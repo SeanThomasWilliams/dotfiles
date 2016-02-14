3ccc2fb2666d58b9f1d01cb4b01b125447885936
Sean Williams <seanthomaswilliams@gmail.com>
2016-02-14 00:45:50 -0500

    Fix YCM install

--- scripts/installycm.sh
+++ scripts/installycm.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 sudo yum -y install automake gcc gcc-c++ kernel-devel cmake
 git submodule update --init --recursive
 cd $DIR/../vim/bundle/YouCompleteMe
