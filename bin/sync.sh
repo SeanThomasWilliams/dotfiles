434a1985b1579bb16a851ee55a6fb8bf89a991f2
Sean Williams <seanthomaswilliams@gmail.com>
2018-05-12 21:15:18 -0400

    Update to allow unsafe sync

--- bin/sync.sh
+++ bin/sync.sh
@@ -1,15 +1,47 @@
 #!/bin/bash
 
-rclone sync \
-    --verbose \
-    --max-delete 0 \
-    --update \
-    $HOME/sync \
-    drive:work
+set -eu
 
-rclone sync \
-    --verbose \
-    --max-delete 0 \
-    --update \
-    drive:work \
-    $HOME/sync
+SAFE_OPTS="--max-delete 0"
+REMOTE="drive:work"
+LOCAL="$HOME/sync"
+
+safesync(){
+    syncpush
+    syncpull
+}
+
+syncpush(){
+    rclone sync $SAFE_OPTS --update --verbose $LOCAL $REMOTE
+}
+
+syncpull(){
+    rclone sync $SAFE_OPTS --update --verbose $REMOTE $LOCAL
+}
+
+while getopts ":f:" opt; do
+    case ${opt} in
+        f)
+            SAFE_OPTS=""
+            if [[ $OPTARG == "push" ]]; then
+                syncpush
+                exit
+            elif [[ $OPTARG == "pull" ]]; then
+                syncpull
+                exit
+            else
+                echo "Error $OPTARG"
+                exit
+            fi
+            ;;
+        \?)
+            echo "Invalid option: $OPTARG" 1>&2
+            ;;
+        :)
+            echo "Invalid option: $OPTARG requires an argument" 1>&2
+            ;;
+    esac
+done
+shift $((OPTIND -1))
+
+safesync
