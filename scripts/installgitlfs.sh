e6ae82de556b4b478e1e4c5038cd5e9e3ffd223e
Sean Williams <seanthomaswilliams@gmail.com>
2018-05-14 02:49:03 +0000

    Update git lfs version

--- scripts/installgitlfs.sh
+++ scripts/installgitlfs.sh
@@ -3,5 +3,5 @@
 cd $HOME/software
 wget -c https://github.com/git-lfs/git-lfs/releases/download/v2.4.0/git-lfs-linux-amd64-2.4.0.tar.gz
 tar -xvf git-lfs-linux-amd64-2.4.0.tar.gz
-cd git-lfs-2.3.4
-sudo ./install
+cd git-lfs-2.4.0
+sudo ./install.sh
