e9407a438c6d961b919c8a4e9e9990a1cf49c6b8
Sean Williams <seanthomaswilliams@gmail.com>
2018-07-06 13:00:30 -0400

    Add boto, netaddr and setuptools for ansible

--- scripts/pip.sh
+++ scripts/pip.sh
@@ -4,6 +4,6 @@ if [[ -d $HOME/anaconda3 ]]; then
     source $HOME/anaconda3/bin/activate
 fi
 
-pip install awscli boto3 botocore pyopenssl ansible==2.4.3 awslogs
+pip install awscli boto boto3 botocore pyopenssl ansible==2.4.3 awslogs netaddr setuptools
 
 pip install --upgrade pip
