186752c4d6db6494ca464b158b9c09d996c320b9
Sean Williams <sean@xeed.io>
2020-06-04 09:47:20 -0700

    Update sync

--- bin/sync.sh
+++ bin/sync.sh
@@ -1,12 +1,34 @@
-#!/bin/bash
+#!/bin/bash -ex
 
-set -u
-
-SAFE_OPTS="--max-delete 0"
+SAFE_OPTS=(--max-delete 0)
 REMOTE="drive:work"
 LOCAL="$HOME/sync"
+RCLONE_VERSION="v1.52.0"
+UNAME_S=$(uname -s | tr '[:upper:]' '[:lower:]')
 
-mkdir -p $LOCAL
+ARCH=""
+case $(uname -m) in
+  i386) ARCH="386" ;;
+  i686) ARCH="386" ;;
+  x86_64) ARCH="amd64" ;;
+esac
+
+mkdir -p "$LOCAL"
+
+init(){
+  export PATH="$HOME/bin:$PATH"
+
+  if ! command -v rclone &> /dev/null; then
+    echo >&2 "Installing rclone..."
+    TMP_DIR=$(mktemp -d)
+    RCLONE_VERSION="rclone-${RCLONE_VERSION}-${UNAME_S}-${ARCH}"
+    ZIP_FILE="${RCLONE_VERSION}.zip"
+    curl --fail -s -L "https://downloads.rclone.org/${RCLONE_VERSION}/${ZIP_FILE}" -o "$TMP_DIR/$ZIP_FILE"
+    unzip -p "$TMP_DIR/$ZIP_FILE" "$RCLONE_VERSION/rclone" > "$HOME/bin/rclone"
+    chmod +x "$HOME/bin/rclone"
+    rm -rf "$TMP_DIR"
+  fi
+}
 
 safesync(){
     syncpush
@@ -14,36 +36,42 @@ safesync(){
 }
 
 syncpush(){
-    rclone sync $SAFE_OPTS --update --verbose $LOCAL $REMOTE
+    rclone sync "${SAFE_OPTS[@]}" --update --verbose "$LOCAL" "$REMOTE"
 }
 
 syncpull(){
-    rclone sync $SAFE_OPTS --update --verbose $REMOTE $LOCAL
+    rclone sync "${SAFE_OPTS[@]}" --update --verbose "$REMOTE" "$LOCAL"
 }
 
 while getopts ":f:" opt; do
-    case ${opt} in
-        f)
-            SAFE_OPTS=""
-            if [[ $OPTARG == "push" ]]; then
-                syncpush
-                exit
-            elif [[ $OPTARG == "pull" ]]; then
-                syncpull
-                exit
-            else
-                echo "Error $OPTARG"
-                exit
-            fi
-            ;;
-        \?)
-            echo "Invalid option: $OPTARG" 1>&2
-            ;;
-        :)
-            echo "Invalid option: $OPTARG requires an argument" 1>&2
-            ;;
-    esac
+  case ${opt} in
+    f)
+      SAFE_OPTS=()
+      case "$OPTARG" in
+        push)
+          syncpush
+          exit
+          ;;
+        pull)
+          syncpull
+          exit
+          ;;
+        *)
+          echo >&2 "Error: Unknown -f '$OPTARG'"
+          exit 1
+          ;;
+      esac
+      ;;
+    \?)
+      echo "Invalid option: $OPTARG" 1>&2
+      ;;
+    :)
+      echo "Invalid option: $OPTARG requires an argument" 1>&2
+      ;;
+  esac
 done
+
 shift $((OPTIND -1))
 
+init
 safesync
