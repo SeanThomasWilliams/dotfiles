84d45286829eaf5746fcd5ade31b5fe99b331e89
Sean Williams <sean@xeed.io>
2025-05-16 12:33:32 -0400

    Add DisplayPort configs, switch to firefox, bootstrap DoD certs in Firefox

--- scripts/install-dod-certs.sh
+++ scripts/install-dod-certs.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+#shellcheck disable=2044
 
 set -eu
 set -o pipefail
@@ -7,6 +8,11 @@ set -x
 CERT_BUNDLE="unclass-certificates_pkcs7_DoD" \
 CERT_URL_PREFIX="https://dl.dod.cyber.mil/wp-content/uploads/pki-pke/zip" \
 CERT_ZIP="/tmp/certs.zip"
+
+CERTDB_DIRS=(
+  "$HOME/snap/firefox/common/.mozilla"
+  "$HOME/.mozilla"
+)
 
 trap 'rm -f $CERT_ZIP' EXIT
 
@@ -28,4 +34,28 @@ elif [[ $ID == "ubuntu" ]]; then
     openssl pkcs7 -print_certs -inform der -out - |\
     sudo awk '/BEGIN CERT/,/END CERT/{ if(/BEGIN CERT/){c++}; out="dod.ca." c ".crt"; print >out}'
   sudo update-ca-certificates
+
+  for certdb_dir in "${CERTDB_DIRS[@]}"; do
+    if [[ -d "$certdb_dir" ]]; then
+      for cert_db in $(find  "$certdb_dir" -type f -name "cert9.db"); do
+        cert_dir=$(dirname "$cert_db")
+        echo >&2 "Updating Cert DB: $cert_db in $cert_dir"
+        for certificate_file in $(find /usr/local/share/ca-certificates -type f -name 'dod.ca.*'); do
+          echo >&2 "Cert file: $certificate_file"
+          certificate_name=$(openssl x509 -in "$certificate_file" -text -noout |\
+            grep -o 'Subject:.*' |\
+            sed 's#.*CN = \(.*\)#\1#' |\
+            tr ' ' '-' |\
+            tr '[:upper:]' '[:lower:]')
+
+          echo >&2 "Mozilla Firefox certificate install '${certificate_name}' in ${cert_dir}"
+          certutil -A \
+            -n "${certificate_name}" \
+            -t "TCu,Cuw,Tuw" \
+            -i "${certificate_file}" \
+            -d sql:"${cert_dir}"
+        done
+      done
+    fi
+  done
 fi
--- scripts/install-tailscale.sh
+++ scripts/install-tailscale.sh
@@ -4,4 +4,15 @@ set -eux
 
 curl -fsSL https://tailscale.com/install.sh | sh
 
-sudo tailscale up --accept-dns
+if [[ ! -f /etc/sysctl.d/99-tailscale.conf ]]; then
+  echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
+  echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
+  sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
+fi
+
+if [[ ! -f /etc/networkd-dispatcher/routable.d/50-tailscale ]]; then
+  printf '#!/bin/sh\n\nethtool -K %s rx-udp-gro-forwarding on rx-gro-list off \n' "$(ip -o route get 8.8.8.8 | cut -f 5 -d " ")" | sudo tee /etc/networkd-dispatcher/routable.d/50-tailscale
+  sudo chmod 755 /etc/networkd-dispatcher/routable.d/50-tailscale
+fi
+
+sudo tailscale up --accept-dns=false
