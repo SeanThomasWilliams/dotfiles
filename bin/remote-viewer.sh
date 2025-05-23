c92c83645921956e125dce74473a154722988175
Sean Williams <sean@xeed.io>
2025-05-23 13:07:40 -0400

    Supprt config variants based on hostname

--- bin/remote-viewer.sh
+++ bin/remote-viewer.sh
@@ -4,7 +4,9 @@
 # Use -1 for class/vendor/product/version to accept any value.
 #  idVendor           0x04e6 SCM Microsystems, Inc.
 #  idProduct          0x5116 SCR331-LC1 / SCR3310 SmartCard Reader
-USB_FILTER="-1,0x04e6,0x5116,-1,1" # Smartcard Reader
+#  idProduct          0x581d SCR3500 C Contact Reader
+
+USB_FILTER="-1,0x04e6,-1,-1,1" # Smartcard Reader
 
 # Take SPICE_VM_HOST from environment variable or default to localhost
 SPICE_VM_HOST="${SPICE_VM_HOST:-localhost}"
